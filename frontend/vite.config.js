2199b92b04cbfae6ab1394a8fcbeadc7b49fe3bc
Zhenrel <iamnotruru@gmail.com>
2026-05-14 15:20:07 +0800

    fix legends again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
     base: "/",
     build: {
         outDir: "dist",
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name]-[hash].js`,
+                chunkFileNames: `assets/[name]-[hash].js`,
+                assetFileNames: `assets/[name]-[hash].[ext]`,
+            },
+        },
     },
 })
